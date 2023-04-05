1b3e7ced6e97366c4f36ba6fb9bc9ae7da835f6b
jrtp22 <jrtp22@gmail.com>
2023-04-04 20:47:51 -0700

    was late to class becus my eye was swollen but i did work earlier and i just got it to put an x on there

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 let player1 = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
 const divs = document.querySelectorAll('.divs');
 divs.forEach(div => {
   div.addEventListener('click', divClick);
 });
-function divClick(){
-
+function divClick(event){
+  const index = event.target.id - 1;
+  if (gameBoard[index] !== '') {
+    return;
+  }
+  event.target.textContent = player1;
+  gameBoard[index] = player1;
 }
-let currentPlayer = 'X';
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
 function restart(){
     window.location.reload();}
